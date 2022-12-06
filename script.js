0e74c1e4cf3917a706aef0847c2d44d9017ab07e
Matt Zachman <mzachman14@gmail.com>
2022-12-05 20:56:21 -0500

    add round logic

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
 function getComputerChoice() {
 
+
     let randInt = Math.floor(Math.random() * 3);
     let choice = "";
 
     if (randInt === 0){
-        choice = "Rock";
+        choice = "rock";
     }
     else if (randInt === 1){
-        choice = "Paper";
+        choice = "paper";
     }
     else{
-        choice = "Scissors";
+        choice = "scissors";
     }
 
  return choice;
 }
 
-function playRound() {
+function getPlayerChoice() {
+    let choice = prompt("Rock, Paper, or Scissors?");
+    choice = choice.toLowerCase();
+    return choice
+}
+
+function playRound(player, computer) {
+    const tie = "You Tied!";
+    const win = "You Won!";
+    const loss = "You Lost!";
+    
+   console.log("Computer picked " + computer);
+
+
+    if (player === computer){
+        return tie;
+    }
+
+    switch (player) {
+        case "rock":
+            switch (computer){
+                case "paper":
+                    return loss;
+                case "scissors":
+                    return win;
+            }
+
+        case "paper":
+            switch (computer){
+                case "rock":
+                    return win;
+                case "scissors":
+                    return loss;
+            }
+        case "scissors":
+            switch (computer){
+                case "rock":
+                    return loss;
+                case "paper":
+                    return win;
+            }
+    }
+   
 
 }
 
 function game() {
 
+
 }
 
-getComputerChoice();
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
